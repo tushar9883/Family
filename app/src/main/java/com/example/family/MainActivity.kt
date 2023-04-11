47a1a3a3576cf4d78f1233e98b9a731596c3c03c
creativehustlers <hustler.jitendrasinh@gmail.com>
2023-04-11 10:41:18 +0530

    Initial Comment

--- app/src/main/java/com/example/family/MainActivity.kt
+++ app/src/main/java/com/example/family/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.family
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.family.fragment.GuardFragment
+import com.example.family.fragment.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         bottomBar.setOnItemSelectedListener {
 
-            if(it.itemId == R.id.nav_guard){
-                inflateGuardFragment()
-            }else if (it.itemId ==R.id.nav_home){
-                inflateHomeFragment()
+            if (it.itemId == R.id.nav_guard) {
+                inflateFragment(GuardFragment.newInstance())
+            } else if (it.itemId == R.id.nav_home) {
+                inflateFragment(HomeFragment.newInstance())
             }
 
 
@@ -25,13 +27,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun inflateHomeFragment() {
 
-    }
-
-    private fun inflateGuardFragment() {
+    private fun inflateFragment(newInstance: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, GuardFragment.newInstance())
+        transaction.replace(R.id.container, newInstance)
         transaction.commit()
     }
 }
